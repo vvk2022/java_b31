d63ac03c754e6eb056bf3a270028a59f897cf8b0
vvk2022 <vaz21120@inbox.ru>
2022-08-03 01:53:54 +0300

    Реализована проверка и выполнение предусловий для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -8,10 +8,7 @@ public class GroupCreationTests extends TestBase{
     @Test
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
-        app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
-        app.getGroupHelper().submitGroupCreation();
-        app.getGroupHelper().returnToGroupPage();
+        app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
         //wd.findElement(By.linkText("Logout")).click();
         //wd.findElement(By.name("user")).clear();
         //wd.findElement(By.name("user")).sendKeys("admin");
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,11 +1,15 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
     @Test
     public void testGroupDeletion() throws Exception {
+        if(!app.getGroupHelper().isThereAGroup()){
+            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        }
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
